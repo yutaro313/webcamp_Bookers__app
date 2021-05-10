622399f1e4282c50b8c99b8085f313e4cb0f0de6
yutaro313 <yuutaro.0313.soccer@gmail.com>
2021-05-10 12:42:17 +0000

    [Finish] 3-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'books#top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books
+ resources :books
 end
